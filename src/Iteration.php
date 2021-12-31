338397aa7bc7daa504d1b2c64a353f93da18668b
sinkcup <sinkcup@gmail.com>
2021-12-31 13:24:42 +0800

    feat: #34 update iteration

--- src/Iteration.php
+++ src/Iteration.php
@@ -28,6 +28,21 @@ class Iteration extends Base
         return $response['Iteration'];
     }
 
+    public function update(array $data)
+    {
+        $this->validate($data, [
+            'ProjectName' => 'string|required',
+            'IterationCode' => 'integer|required',
+            'Name' => 'nullable|string',
+            'Goal' => 'nullable|string',
+            'Assignee' => 'nullable|integer',
+            'StartAt' => 'nullable|date_format:Y-m-d',
+            'EndAt' => 'nullable|date_format:Y-m-d|after:StartAt',
+        ]);
+        $response = $this->core->request('ModifyIteration', $data);
+        return $response['Iteration'];
+    }
+
     public function delete(array $data)
     {
         $this->validate($data, [
